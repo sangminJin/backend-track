03a2bbb8285ff8d8f506677346768997ce188ef4
sangmin <jin0849@naver.com>
2022-05-08 16:42:14 +0900

    Feat: 주문 이력 조회
    현재 회원의 주문 이력 조회
    - 주문 이력 페이지당 6개씩 노출되도록 페이징
    - 주문취소 기능

--- project-lion-web/src/main/java/com/shop/projectlion/web/orderhist/dto/OrderHistDto.java
+++ project-lion-web/src/main/java/com/shop/projectlion/web/orderhist/dto/OrderHistDto.java
@@ -1,12 +1,18 @@
 package com.shop.projectlion.web.orderhist.dto;
 
 import com.shop.projectlion.domain.order.constant.OrderStatus;
-import lombok.*;
+import com.shop.projectlion.domain.order.entity.Orders;
+import com.shop.projectlion.domain.orderitem.entity.OrderItem;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
-@Getter @Setter
+@Getter
+@Setter
 @Builder
 public class OrderHistDto {
 
@@ -15,16 +21,40 @@ public class OrderHistDto {
     private OrderStatus orderStatus;
     private int totalPrice;
     private int totalDeliveryFee;
-
     private List<OrderItemHistDto> orderItemHistDtos;
 
-    @Getter @Setter
+    public static OrderHistDto of(Orders orders) {
+        List<OrderItemHistDto> orderItemHistDtos = OrderItemHistDto.of(orders.getOrderItems());
+
+        return OrderHistDto.builder()
+                .orderId(orders.getId())
+                .orderTime(orders.getOrderTime())
+                .orderStatus(orders.getOrderStatus())
+                .totalPrice(orders.getTotalPrice())
+                .totalDeliveryFee(orders.getOrderItems().get(0).getItem().getDelivery().getDeliveryFee())
+                .orderItemHistDtos(orderItemHistDtos)
+                .build();
+    }
+
+    @Getter
+    @Setter
     @Builder
     public static class OrderItemHistDto {
         private String itemName;
         private int count;
         private int orderPrice;
         private String imageUrl;
+
+        public static List<OrderItemHistDto> of(List<OrderItem> orderItems) {
+            return orderItems.stream()
+                    .map(orderItem -> OrderItemHistDto.builder()
+                            .itemName(orderItem.getItem().getItemName())
+                            .count(orderItem.getCount())
+                            .orderPrice(orderItem.getOrderPrice())
+                            .imageUrl(orderItem.getItem().getItemImages().get(0).getImageUrl())
+                            .build())
+                    .collect(Collectors.toList());
+        }
     }
 
 }
